4c1cb4087216240fa8d3ea04c30668c33e6548b4
Adrian <adrianvalverde45@gmail.com>
2022-10-07 23:05:47 -0400

    commit tree 1d28720714d68efea965360a6949f3fdc5e5d704

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+/*
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+}
+
+element.append(ul);
+*/
+
+/*
+const bodyBody = document.getElementsByTagName("body")[0];
+const mainMain = bodyBody.getElementsById("main");
+bodyBody.removeChild(mainMain);
+*/
+
+// const body = document.getElementsByTagName("body")[0];
+const main = document.querySelector("main")
+main.remove();
+
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Adrian is the champion"
